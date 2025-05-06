4e9026e6102aca1331a5870dfb1eb9d016b4b432
nicolasutec <nicolas.vasquezdevel@utec.edu.pe>
2025-05-06 17:11:33 -0500

    feat: crear company

--- hackaton1/src/main/java/org/dbp/hackaton/hackaton1/domain/company.java
+++ hackaton1/src/main/java/org/dbp/hackaton/hackaton1/domain/company.java
@@ -1,4 +1,23 @@
 package org.dbp.hackaton.hackaton1.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class company {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String ruc;
+    private LocalDate subscriptionDate;
+    private boolean active;
+
+    @OneToOne
+    private User admin;
+
+    @OneToMany(mappedBy = "company")
+    private List<User> users;
+
+    @OneToMany(mappedBy = "company")
+    private List<CompanyRestriction> restrictions;
 }
